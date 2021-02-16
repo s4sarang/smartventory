59e3fdfd63f9be68f5af86a820f5b1ad9844575a
Sarang Girdhari <saranggirdhari@gmail.com>
2021-02-17 01:03:50 +0530

    feat: 🎸 data fetch from express

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
-import express from 'express';
+const express = require('express');
+const assets = require('./data/assets.json');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('APIs are running');
+});
+
+app.get('/api/assets', (req, res) => {
+  res.send(assets);
+});
+
+app.get('/api/assets/:dlink', (req, res) => {
+  const asset = assets.find((asset) => asset.link === req.params.dlink);
+  res.send(asset);
+});
 
 app.listen(5000, console.log('Hello World!'));
