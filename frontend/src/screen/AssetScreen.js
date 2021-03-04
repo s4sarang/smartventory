deceac1507fd8f971a3588f04cfacd02b86d9a76
Sarang Girdhari <saranggirdhari@gmail.com>
2021-03-05 00:00:54 +0530

    feat: Add 'Add to cart' list group

--- frontend/src/screen/AssetScreen.js
+++ frontend/src/screen/AssetScreen.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { ListGroup, Col, Row, Button, Image } from 'react-bootstrap';
+import {
+  ListGroup,
+  Col,
+  Row,
+  Button,
+  Image,
+  ListGroupItem,
+} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { listAssetDetails } from '../actions/assetActions';
 import Loader from '../components/Loader';
@@ -31,59 +38,31 @@ const AssetScreen = ({ match }) => {
           </Col>
           <Col xs={6} md={4}>
             <ListGroup>
-              <ListGroup.Item variant='success'>
+              <ListGroup.Item variant='light'>
                 Brand : {asset.brand}
               </ListGroup.Item>
-              <ListGroup.Item variant='light'>
+              <ListGroup.Item variant='success'>
                 Model : {asset.model}
               </ListGroup.Item>
-              <ListGroup.Item variant='success'>
-                Launch Year : {asset.launch}
-              </ListGroup.Item>
               <ListGroup.Item variant='light'>
-                Price : {asset.price}
+                Launch Year : {asset.launch}
               </ListGroup.Item>
               <ListGroup.Item variant='success'>
                 Category : {asset.category}
               </ListGroup.Item>
             </ListGroup>
           </Col>
-          <Col xs={6} md={4}></Col>
+          <Col xs={6} md={2}>
+            <ListGroup>
+              <ListGroupItem>Price: {asset.price}</ListGroupItem>
+              <ListGroupItem>Stock: {asset.count}</ListGroupItem>
+              <Button className='my-3' variant='secondary'>
+                Add to cart
+              </Button>
+            </ListGroup>
+          </Col>
         </Row>
       )}
-
-      {/* <ListGroup className='my-3'>
-        <ListGroup.Item>IMEI:</ListGroup.Item>
-        <ListGroup.Item>
-          {assets.map((a) => (asset.link === a.link ? a.unit.serialno : ''))}
-        </ListGroup.Item>
-        <ListGroup.Item>Team:</ListGroup.Item>
-        <ListGroup.Item>
-          {assets.map((a) => (asset.link === a.link ? a.unit.team : ''))}
-        </ListGroup.Item>
-        <ListGroup.Item>Accessories:</ListGroup.Item>
-        <ListGroup.Item>
-          {assets.map((a) => (asset.link === a.link ? a.unit.accessories : ''))}
-        </ListGroup.Item>
-        <ListGroup.Item>Reason:</ListGroup.Item>
-        <ListGroup.Item>
-          {assets.map((a) => (asset.link === a.link ? a.unit.reason : ''))}
-        </ListGroup.Item>
-      </ListGroup> */}
-      {/* <ListGroup>
-        {assets.map((a, id) =>
-          asset.link === a.link ? (
-            <ListGroup horizontal={a.unit.serialno} className='my-2' key={id}>
-              <ListGroup.Item>Serial No: {a.unit.serialno} </ListGroup.Item>
-              <ListGroup.Item>Team: {a.unit.team}</ListGroup.Item>
-              <ListGroup.Item>Accessories: {a.unit.accessories}</ListGroup.Item>
-              <ListGroup.Item>Reason: {a.unit.reason}</ListGroup.Item>
-            </ListGroup>
-          ) : (
-            ''
-          )
-        )}
-      </ListGroup> */}
     </>
   );
 };
