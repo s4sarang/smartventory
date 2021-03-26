7732ec9b2a39f900c54b4abe371bce88b1f633af
Sarang Girdhari <saranggirdhari@gmail.com>
2021-03-26 19:22:00 +0530

    feat: Add requests model

---
+++ backend/models/requestsModel.js
@@ -0,0 +1,67 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const requestsSchema = mongoose.Schema(
+  {
+    user: {
+      type: Mongoose.Schema.types.ObjectId,
+      required: true,
+      ref: 'User',
+      unique: true,
+    },
+    requestsItems: [
+      {
+        link: { type: String, required: true },
+        name: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        count: { type: Number, required: true },
+        qty: { type: Number, required: true },
+        asset: {
+          type: Mongoose.Schema.types.ObjectId,
+          required: true,
+          ref: 'Assets',
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      pinCode: { type: String, required: true },
+      state: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Requests = mongoose.model('Requests', requestsSchema);
+
+export default Requests;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,7 +28,7 @@ const userSchema = mongoose.Schema(
     },
   },
   {
-    timestamp: true,
+    timestamps: true,
   }
 );
 
